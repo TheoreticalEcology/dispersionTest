e2871fdd4ad112d21c141752d090840ae51223ac
melina-leite <melina.leite@ib.usp.br>
2024-11-14 12:59:27 +0000

    glm graphs KS test and type I errors

--- 01_glm_pearson_OLD.R
+++ 01_glm_pearson_OLD.R
@@ -5,51 +5,62 @@
 library(DHARMa)
 library(tidyverse)
 library(here)
+library(patchwork)
 
 #####################################
 ##### Instructions & Simulating #####
 #####################################
 
-# 1) Simulating 1000 Poisson datasets with diferent parameters (see below)
-# 2) fitting them to correct GLM model -> 
+# 1) Simulating 1000 Poisson datasets with different sample sizes and intercepts
+# 2) fitting them to correct GLM model
+
+## KS GOF test for Pearson Stats and Chisquared
+
 # 3) calculating pearson statistics of the pearson residuals (sum(resˆ2)) -> 
 # 4) comparing distribution of these statistics with the Chisquared distribution with the same DF.
 
-#Simulting Poisson:  
-#  - varying N 10,20,30,40,50,100,500. 
-#  - varying Lambda/intercept 0,1,2,4,8,16,32. 
 
-# function to varying N
+## Type I error rates for Dispersion tests
+# 5) calculating type I error rate for P-chisq, DHARMa default, DHARMa refit
+
+
+# function to varying sampleSize
 calculateStatistics <- function(control = 10){
+  # data
   testData <- DHARMa::createData(sampleSize = control,
                                  numGroups = 1,
                                  family = poisson())
-  
+  # model
   fittedModel <- stats::glm(observedResponse ~ Environment1, 
                             data = testData, family = poisson()) 
-  
-  pearson <- residuals(fittedModel, "pearson")
-  
+  #results
   out <- list()
-  out$pearson.stat <- sum(pearson^2)
-  out$rdf <- df.residual(fittedModel)
-  out$disp <- out$pearson.stat/out$rdf
-  out$p.value <- DHARMa::testDispersion(fittedModel, 
-                                        type="PearsonChisq")$p.value
-  return(out)
+  
+  # pearson residual
+  out$Pear.stat <- sum(residuals(fittedModel, "pearson")^2)
+  out$Pear.p.val <- testDispersion(fittedModel, plot = F, 
+                                   type="PearsonChisq")$p.value
+  # DHARMa default residuals
+  res <- simulateResiduals(fittedModel)
+  out$DHA.p.val<- testDispersion(res, type = "DHARMa",plot = F)$p.value
+
+  # DHARMa refit residuals -> bootstrapped Pearson
+  res <- simulateResiduals(fittedModel, refit=T)
+  out$Ref.p.val <- testDispersion(res, plot = F, type = "DHARMa")$p.value
+  return(unlist(out))
   }
 
-# varying intercept in a loop (don't know how to have 2 pars in runBenchmarks)
-intercept <- c(0,1,2,4,8,16,32)
+# varying intercept in a loop 
+intercept <- c(-3,-1,0,1,4)
 
 result <- list()
 out.out <- list()
 for (i in intercept){
-  sampleSize = c(10,20,30,40,50,100,500)
+  sampleSize = c(10,20,50,100,500)
   
   out <- runBenchmarks(calculateStatistics, controlValues = sampleSize,
                        intercept = i,
-                       nRep=1000, parallel = 7)
+                       nRep=1000, parallel = 15)
  
   chistats <- as.data.frame(apply(as.data.frame(out$simulations), 2, unlist ))
   chistats$intercept <- i
@@ -73,13 +84,14 @@ ks.d <- list()
 for (i in sampleSize){
   for (n in intercept){
     dat <- result[result$controlValues == i & result$intercept == n,]
-    ks <- ks.test(dat$pearson.stat,rchisq(100000,df=i-2))
+    ks <- ks.test(dat$Pear.stat,rchisq(100000,df=i-2))
     ks.p <- rbind(ks.p,ks$p.value)
     ks.d <- rbind(ks.d,ks$statistic)
   }
 }
 
-res.ks <- data.frame(sampleSize = rep(sampleSize, each=7),
+res.ks <- data.frame(sampleSize = rep(sampleSize, each =
+                                        length(unique(sampleSize))),
                      intercept = rep(intercept),
                      ks.pvalue = unlist(ks.p),
                      ks.d = unlist(ks.d))
@@ -98,7 +110,7 @@ ggsave(here("figures", "1_glm_pearson_ksP-values.jpeg"))
 
 #### Distribution plots save as PDF ####
 
-
+# DENSITY
 pdf(here("figures", "1_glm_pearson_distributions.pdf"), width=15, height=15)
 par(mfrow=c(length(sampleSize),length(intercept)))
 
@@ -108,11 +120,11 @@ for(i in sampleSize){
    
    ks <- res.ks[res.ks$sampleSize == i & res.ks$intercept == n,]
    
-   plot(density(figs2$pearson.stat), ann=F, axes=F)
+   plot(density(figs2$Pear.stat), ann=F, axes=F)
   if(ks$ks.pvalue<0.05) rect(par("usr")[1],par("usr")[3],par("usr")[2],
                            par("usr")[4], col = "#F7C6C6")
   par(new=T)
-  plot(density(figs2$pearson.stat), 
+  plot(density(figs2$Pear.stat), 
        main = paste0("sampleSize=",i, "; intercept=",n),
        xlab = paste0("KS: D=", round(ks$ks.d,3), 
                      ", p=", round(ks$ks.pvalue,3)))
@@ -123,6 +135,36 @@ dev.off()
 
 # caption: Black line: Distribution of the Pearson statistics (sum of squares of the Pearson Residuals) from 1000 simulated Poisson data fitted to glm Poisson models. Red line: 100,000 sampled values from a Chisquared distribution with the same residuals degrees of freedm of the models. Red panels are the ones with KS significative results.  
 
+# CUMULATIVE DISTRIB
+pdf(here("figures", "1_glm_pearson_distributions_ecdf.pdf"), width=15, height=15)
+par(mfrow=c(length(sampleSize),length(intercept)))
+
+for(i in sampleSize){
+  for (n in intercept){
+    figs2 <- result[result$controlValues == i & result$intercept == n,]
+    
+    ks <- res.ks[res.ks$sampleSize == i & res.ks$intercept == n,]
+    
+    plot(ecdf(figs2$Pear.stat), ann=F, axes=F)
+    if(ks$ks.pvalue<0.05) rect(par("usr")[1],par("usr")[3],par("usr")[2],
+                               par("usr")[4], col = "#F7C6C6")
+    par(new=T)
+    plot(ecdf(figs2$Pear.stat), 
+         main = paste0("sampleSize=",i, "; intercept=",n),
+         xlab = paste0("KS: D=", round(ks$ks.d,3), 
+                       ", p=", round(ks$ks.pvalue,3)))
+    lines(ecdf(rchisq(100000,df=i-2)),col=2)
+  }
+}
+dev.off()
+
+
+
+
+
+
+
+
 #### Type I error rate for the dispersion tests #####
 
 
@@ -132,17 +174,43 @@ for (i in 1:length(out.out)) {
 }
 
 prop.sig$sampleSize = prop.sig$controlValues
-prop.sig$intercept = rep(intercept, each=7)
+prop.sig$intercept = rep(intercept, each=length(unique(intercept)))
 
 ggplot(prop.sig, aes(y=rev(as.factor(sampleSize)),x=as.factor(intercept), 
-                     fill=p.value)) +
+                     fill=Pear.p.val)) +
+  geom_tile() +
+  scale_y_discrete("sampleSize", labels=c(500,100,50,20,10)) +
+  scale_x_discrete("intercept", position = "top")+
+  geom_text(aes(label=round(Pear.p.val,3))) +
+  scale_fill_gradient2(name="type I", low=4, high=2, midpoint=0.05,
+                       mid="white") +
+  ggtitle("Proportion of significant Pvalues for Pearson-Chisquare dispersion test") +
+
+ggplot(prop.sig, aes(y=rev(as.factor(sampleSize)),x=as.factor(intercept), 
+                     fill=DHA.p.val)) +
+  geom_tile() +
+  scale_y_discrete("sampleSize", labels=c(500,100,50,20,10)) +
+  scale_x_discrete("intercept", position = "top")+
+  geom_text(aes(label=round(DHA.p.val,3))) +
+  scale_fill_gradient2(name="type I", low=4, high=2, midpoint=0.05,
+                       mid="white") +
+  ggtitle("Proportion of significant Pvalues for DHARMa residuals test")+
+
+ggplot(prop.sig, aes(y=rev(as.factor(sampleSize)),x=as.factor(intercept), 
+                     fill=Ref.p.val)) +
   geom_tile() +
   scale_y_discrete("sampleSize", labels=c(500,100,50,40,30,20,10)) +
   scale_x_discrete("intercept", position = "top")+
-  geom_text(aes(label=round(p.value,3))) +
-  scale_fill_gradient2(name="type I", low="white", high=2, midpoint=0.05) +
-  ggtitle("Proportion of significant Pvalues for Pearson-Chisquare dispersion test")
+  geom_text(aes(label=round(Ref.p.val,3))) +
+  scale_fill_gradient2(name="type I", low=4, high=2, midpoint=0.05,
+                       mid="white") +
+  ggtitle("Proportion of significant Pvalues for DHARMa refit Pearson residuals") +
+  plot_layout(ncol=1,guides = "collect")
+
+
 ggsave(here("figures", "1_glm_pearson_typeI_dispersion.jpeg"))
+
+
 
 
 dat <- left_join(res.ks, prop.sig, by=c("sampleSize","intercept" ))
---
+++ 2_glm_dispersion_type1.R
@@ -0,0 +1,175 @@
+### Dispersion Tests Project
+## Melina Leite
+# Nov 24
+
+library(DHARMa)
+library(tidyverse); library(cowplot);
+theme_set(theme_cowplot())
+library(here)
+library(patchwork)
+
+#####################################
+##### Instructions & Simulating #####
+#####################################
+
+# 1) Simulating 1000 Poisson datasets with different sample sizes and intercepts
+#       - sampleSize: c(10,50,100,500)
+#       - intercept:  c(-3,-1,0,2,4)
+# 2) fitting them to correct GLM models
+# 5) calculating type I error rate for 
+#     - Pearson-chisq, 
+#     - DHARMa default (simulated residuals)
+#     - DHARMa refit (boostrapped Pearson residuals)
+
+
+# function to varying sampleSize
+calculateStatistics <- function(control = 10){
+  # data
+  testData <- DHARMa::createData(sampleSize = control,
+                                 numGroups = 1,
+                                 family = poisson())
+  # model
+  fittedModel <- stats::glm(observedResponse ~ Environment1, 
+                            data = testData, family = poisson()) 
+  #results
+  out <- list()
+  
+  # pearson residual
+  out$Pear.p.val <- testDispersion(fittedModel, plot = F, 
+                                   type="PearsonChisq")$p.value
+  # DHARMa default residuals
+  res <- simulateResiduals(fittedModel)
+  out$DHA.p.val<- testDispersion(res, type = "DHARMa",plot = F)$p.value
+  
+  # DHARMa refit residuals -> bootstrapped Pearson
+  res <- simulateResiduals(fittedModel, refit=T)
+  out$Ref.p.val <- testDispersion(res, plot = F, type = "DHARMa")$p.value
+  return(unlist(out))
+}
+
+# varying intercept in a loop 
+intercept <- c(-3,-1,0,1,4)
+sampleSize = c(10,50,100,500)
+
+out.out <- list()
+for (i in intercept){
+
+  out <- runBenchmarks(calculateStatistics, controlValues = sampleSize,
+                       intercept = i,
+                       nRep=1000, parallel = 15)
+  out.out[[length(out.out) + 1]] <- out
+}
+
+names(out.out) <- intercept
+
+# saving sim results
+save(out.out,sampleSize,intercept, file=here("data", 
+                                                    "2_glm_dispersion_type1.Rdata"))
+
+#load(here("data", "2_glm_dispersion_type1.Rdata"))
+
+
+# prep data
+prop.sig <- list()
+for (i in 1:length(out.out)) {
+  prop.sig <- rbind(prop.sig, out.out[[i]]$summaries$propSignificant)
+}
+prop.sig$sampleSize = prop.sig$controlValues
+prop.sig$intercept = rep(intercept, each=length(unique(sampleSize)))
+
+prop.sig <- prop.sig %>% pivot_longer(c(Pear.p.val,DHA.p.val, Ref.p.val), 
+                                      names_to = "tests", values_to = "p.val")
+prop.sig$binom.test <- binom.test(prop.sig$p.val,n = 1000,p=0.05)
+
+simuls <- list()
+for (i in 1:length(out.out)) {
+  sim <- out.out[[i]]$simulations
+  sim$intercept <- names(out.out)[i]
+  simuls <- rbind(simuls,sim)
+}
+names(simuls)[names(simuls)=="controlValues"] <- "sampleSize"
+
+simuls <- simuls %>% pivot_longer(1:3, names_to = "test", values_to = "p.val") %>%
+  group_by(sampleSize,intercept, test) %>%
+  summarise(p.sig = sum(p.val<0.05,na.rm=T),
+            nsim = length(p.val[!is.na(p.val)]) )
+simuls$prop.sig <- simuls$p.sig/simuls$nsim
+for (i in 1:nrow(simuls)) {
+  btest <- binom.test(simuls$p.sig[i], n=simuls$nsim[i], p=0.05)
+  simuls$p.bin0.05[i] <- btest$p.value 
+  simuls$conf.low[i] <- btest$conf.int[1]
+  simuls$conf.up[i] <- btest$conf.int[2]
+  }
+simuls$intercept <- as.factor(as.numeric(simuls$intercept))
+
+#######################################
+##### Figures and summary results #####
+#######################################
+
+#### Type I error rate for the dispersion tests #####
+
+simuls$asterisk <- paste0(round(simuls$prop.sig,3),ifelse(simuls$p.bin0.05 < 0.05,"*",""))
+
+
+ggplot(simuls, aes(y=rev(as.factor(sampleSize)), x=as.factor(intercept), 
+                   fill=prop.sig)) +
+  facet_grid(rows = vars(test)) +
+  geom_tile() +
+  scale_y_discrete("sampleSize", labels=c(500,100,50,20,10)) +
+  scale_x_discrete("intercept", position = "top") +
+  geom_text(aes(label=asterisk)) +
+  scale_fill_gradient2(name="type I", low=4, high=2, midpoint=0.05,
+                       mid="white") +
+  ggtitle("Type I error rates") 
+
+
+
+
+
+ggplot(simuls, aes(y = prop.sig, x=intercept, col=test)) +
+  facet_wrap(~sampleSize) +
+  geom_point(position = position_dodge(width=0.8)) +
+  geom_errorbar(position = position_dodge(width=0.8),
+    aes(ymin=conf.low, ymax=conf.up), width = 0.1)+
+  geom_line(aes(x=as.numeric(intercept)),
+            position = position_dodge(width=0.8))+
+  geom_hline(yintercept = 0.05, linetype="dotted") 
+
+
+
+ggplot(simuls, aes(y = prop.sig, x=as.factor(sampleSize), col=intercept)) +
+  facet_wrap(~test, labeller = as_labeller(c(`DHA.p.val`="DHARMa default" ,
+                                             `Pear.p.val`="Pearson-Chisq" ,
+                                             `Ref.p.val`="DHARMa refit"))) +
+  geom_point(position = position_dodge(width=0.8)) +
+  geom_errorbar(position = position_dodge(width=0.8),
+                aes(ymin=conf.low, ymax=conf.up), width = 0.1)+
+  geom_hline(yintercept = 0.05, linetype="dotted")+
+  ggtitle("Dispersion tests: Type I error rates for GLM Poisson",
+          sub = "95% CIs with exact Binomial tests; 1000 simulations") +
+  xlab("Sample size") +
+  ylab("Type I error") +
+  theme(panel.background  = element_rect(color = "black"))
+ggsave(here("figures", "2_glm_dispersion_type1.jpeg"), width=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
