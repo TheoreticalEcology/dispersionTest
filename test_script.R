3459c13e4b95fe5482ad645f3c706beed545d399
melina-leite <melina.leite@ib.usp.br>
2025-03-04 09:34:11 +0100

    fixing problem pbs

--- test_script.R
+++
@@ -1,8 +0,0 @@
-library(DHARMa)
-
-testData <- createData(overdispersion = 0,
-                       sampleSize = 1000,
-                       intercept = 0,
-                       numGroups = 10,
-                       randomEffectVariance = 0,
-                       family = poisson())
