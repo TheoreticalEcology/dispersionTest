4cf6f9b5561b73388904e56379d8d9b5a5422aa9
melina-leite <melina.leite@ib.usp.br>
2025-03-10 17:43:06 +0100

    Update 8_glmm_approxDFpearson_results.R

--- code_results/8_glmm_approxDFpearson_results.R
+++ code_results/8_glmm_approxDFpearson_results.R
@@ -15,7 +15,7 @@ source(here("code_results", "plotColors.R"))
 
 load(here("data", "8_approximateDFpearson.Rdata")) # simulated data
 
-(tspent <- map_dbl(out.pois, "time")) # seconds
+(tspent <- map_dbl(out.pois, "time")) # minutes
 
 
 #simulations
@@ -42,9 +42,64 @@ p.pois %>%
   ggplot(aes(x=overdispersion, y=prop.sig, col=test, linetype=ngroups, shape=ngroups))+
   geom_point(alpha=0.7) + geom_line(alpha=0.7) +
   facet_grid(sampleSize~intercept) +
-  geom_hline(yintercept = 0.5, linetype="dotted") +
-  ggtitle("Poisson", subtitle = "100 sim") +
+  geom_hline(yintercept = c(0.05,0.5), linetype="dotted") +
+  ylim(0,1)+
+  ggtitle("Poisson", subtitle = "1000 sim") +
   theme(panel.background = element_rect(color="black"),
         legend.position = "bottom") + 
   guides(color=guide_legend(nrow=2, byrow=TRUE))
 #ggsave(here("figures", "5_glmmPois_powerALL.jpeg"), width=12, height = 15)
+
+
+
+###### Type 1 error #####
+
+p.pois %>% filter(overdispersion == 0) %>% ungroup() %>%
+  mutate(ngroups = fct_relevel(ngroups, "10", "50", "100")) %>%
+  ggplot(aes(x=sampleSize, y=prop.sig, col=intercept))+
+  geom_point( position = position_dodge(width = 0.9))+
+  geom_hline(yintercept = 0.05, linetype="dotted")+
+  geom_line(aes(x=as.numeric(sampleSize)),
+            position = position_dodge(width = 0.9))+
+  facet_grid(ngroups~test)+
+  theme(panel.background = element_rect(color="black"),
+        axis.text.x = element_text(angle=45, hjust=1),
+        legend.position = c(0.01,0.9)) 
+#ggsave(here("figures", "5_glmmPois_type1.jpeg"), width=12, height = 8)
+
+
+
+
+
+
+###### dispersion stat ####
+d.pois <- simuls.pois %>% dplyr::select(ends_with("dispersion"), replicate, ngroups,
+                                        overdispersion, intercept, sampleSize) %>%
+  pivot_longer(1:4, names_to = "test", values_to = "dispersion") %>%
+  group_by(sampleSize, ngroups, intercept,overdispersion, test) %>%
+  summarise(mean.stat = mean(dispersion, na.rm=T))
+d.pois$intercept <- fct_relevel(d.pois$intercept, "-1.5", "0", "1.5")
+d.pois$ngroups <- fct_relevel(d.pois$ngroups, "10", "50", "100")
+d.pois$sampleSize <- as.factor(as.numeric(d.pois$sampleSize))
+
+
+# all groups
+d.pois %>% filter(test != "Pear.stat.dispersion",
+                  mean.stat <1000) %>%
+  ggplot( aes(x=overdispersion, y=mean.stat, col=test, linetype=ngroups, shape=ngroups))+
+  geom_point(alpha=0.7) + geom_line(alpha=0.7) +
+  scale_y_log10()+
+  facet_grid(sampleSize~intercept, scales="free") +
+  annotate("rect", xmin = 0, xmax = 1, ymin = 0.5, ymax = 1,
+           alpha = .1,fill = "red")+
+  geom_hline(yintercept = 1, linetype="dotted", col="gray")+
+  ggtitle("Poisson: dispersion statistics", subtitle = "100 sim") +
+  theme(panel.background = element_rect(color="black"),
+        legend.position = "bottom") + 
+  guides(color=guide_legend(nrow=2, byrow=TRUE))
+#ggsave(here("figures", "5_glmmPois_dispersionStatsALL.jpeg"), width=12, height = 15)
+
+
+
+
+
