85a342b0b68947025f9c1ff82be703b1ed3b557b
melina-leite <melinasouzaleite@gmail.com>
2025-10-31 16:44:33 +0100

    new options for figure 6

--- 9_summary_radarChart.R
+++ 9_summary_radarChart.R
@@ -6,6 +6,8 @@
 library(tidyverse)
 #install.packages("fmsb")
 library(fmsb)
+library(cowplot)
+theme_set(theme_cowplot())
 
 # scores 1 to 3
 # df <- data.frame(
@@ -19,15 +21,124 @@ library(fmsb)
 # )
 
 # scores 1 to 5
-df <- data.frame(
+df <- tibble(
   test = c("parametric Pearson Residuals", "nonparametric Pearson Residuals",
            "simulation-based response variance"),
-  speed = c(5,2,3),
-  GLMs = c(5,5,5),
-  GLMs_special_cases = c(1,4,3),
-  GLMMs_few_REgroups = c(2,5,4),
-  GLMMs_many_REgroups = c(1,4,4)
+  Speed = c(5,2,3),
+  GLM = c(5,5,5),
+  `GLM (small-data)` = c(1,4,3),
+  `GLMM (few RE groups)` = c(2,5,4),
+  `GLMM (many RE groups)` = c(1,4,4)
 )
+
+# circle plot
+
+df %>% pivot_longer(2:6, names_to = "variable", values_to = "value") %>%
+  mutate(test = fct_relevel(test,"simulation-based response variance",
+                           "nonparametric Pearson Residuals",
+                           "parametric Pearson Residuals"),
+         variable = fct_relevel(variable,"GLM","GLM (small-data)",
+                                "GLMM (few RE groups)", "GLMM (many RE groups)", 
+                                "Speed" )) %>%
+  mutate(test.n = as.numeric(test),
+         variable.n = as.numeric(variable)) -> df2
+
+ggplot(df2, aes(x=variable.n, y=test.n, size=value, fill=value, col=value))+
+  geom_point(shape=21,) +
+  scale_radius(range = c(3, 25),guide = "none" ) +
+  scale_fill_gradient(low="coral1", high="aquamarine3", name = "Score") +
+  scale_color_gradient(low="coral1", high="aquamarine3", name = "Score") +
+  scale_y_continuous(name = "", breaks=1:3, limits=c(0.6,3.4),
+                     labels= c("Parametric \n Pearson  residuals",
+                               "Nonparametric \n Pearson residuals",
+                               "Simulation-based \n response variance")) +
+  scale_x_continuous(name="",  breaks=1:5, limits=c(0.7,5.3),
+                     labels= c("GLM \n", "GLM \n (\"small-data\")",
+                               "GLMM \n (few RE groups)", "GLMM \n (many RE groups)",
+                               "Speed \n"),
+                     position = "top") +
+  geom_vline(xintercept = c(1.5,2.5,3.5,3.5,4.5), color="white", size=1.5)+
+  geom_hline(yintercept = c(1.5,2.5), color="white", size=1.5)+
+  theme(panel.background = element_rect(colour="white", fill="gray95"),
+        axis.line=element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "bottom")
+
+
+
+
+ggsave(filename = "figures/9_testsEvaluation.jpeg", height = 5, width = 9)
+
+
+
+
+# scores 1 to 3
+df <- tibble(
+  test = c("parametric Pearson Residuals", "nonparametric Pearson Residuals",
+           "simulation-based response variance"),
+  Speed = c(3,2,1),
+  GLM = c(3,3,3),
+  `GLM (small-data)` = c(1,2,1),
+  `GLMM (few RE groups)` = c(2,3,3),
+  `GLMM (many RE groups)` = c(1,3,2)
+)
+
+# circle plot
+
+df %>% pivot_longer(2:6, names_to = "variable", values_to = "value") %>%
+  mutate(test = fct_relevel(test,"simulation-based response variance",
+                            "nonparametric Pearson Residuals",
+                            "parametric Pearson Residuals"),
+         variable = fct_relevel(variable,"GLM","GLM (small-data)",
+                                "GLMM (few RE groups)", "GLMM (many RE groups)", 
+                                "Speed" )) %>%
+  mutate(test.n = as.numeric(test),
+         variable.n = as.numeric(variable)) -> df2
+
+df2$value.t <- "++"
+df2$value.t[df2$value == 1] <- "-"
+df2$value.t[df2$value == 2] <- "+"
+
+ggplot(df2, aes(x=variable.n, y=test.n, fill=value, col=value))+
+  geom_text(aes(label= value.t), size=15)+
+  #geom_point(shape=21,size=20) +
+  #scale_radius(range = c(3, 25),guide = "none" ) +
+  scale_fill_gradient(low="coral1", high="aquamarine3", name = "Score") +
+  scale_color_gradient(low="coral1", high="aquamarine3", name = "Score") +
+  scale_y_continuous(name = "", breaks=1:3, limits=c(0.6,3.4),
+                     labels= c("Parametric \n Pearson  residuals",
+                               "Nonparametric \n Pearson residuals",
+                               "Simulation-based \n response variance")) +
+  scale_x_continuous(name="",  breaks=1:5, limits=c(0.7,5.3),
+                     labels= c("GLM \n", "GLM \n (\"small-data\")",
+                               "GLMM \n (few RE groups)", "GLMM \n (many RE groups)",
+                               "Speed \n"),
+                     position = "top") +
+  geom_vline(xintercept = c(1.5,2.5,3.5,3.5,4.5), color="white", size=1.5)+
+  geom_hline(yintercept = c(1.5,2.5), color="white", size=1.5)+
+  theme(panel.background = element_rect(colour="white", fill="gray95"),
+        axis.line=element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "none")
+
+
+
+
+ggsave(filename = "figures/9_testsEvaluation_sign.jpeg", height = 4.5, width = 9)
+
+
+
+
+
+
+
+
+
+
+
+
+#### radarChart 
+
 
 dft <- df %>% pivot_longer(2:6,
                            names_to = "variables",
@@ -84,3 +195,4 @@ radarchart(dfbasic[c(1,2,5),],
            plty = 1,        # Line type for each line
            pfcol = areas[3],
            title="Simulation-based response variance test")
+
