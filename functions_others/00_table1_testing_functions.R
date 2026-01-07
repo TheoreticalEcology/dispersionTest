3b73988940564501d4265d4b5f837f38a803ac2f
melina-leite <melinasouzaleite@gmail.com>
2026-01-07 14:54:57 +0100

    new test functions for table 1

--- functions_others/00_table1_testing_functions.R
+++ functions_others/00_table1_testing_functions.R
@@ -13,7 +13,7 @@ glm1 <- glm(observedResponse ~ Environment1, data = testData2,
             family = "poisson")
 glmnb1 <- MASS::glm.nb(observedResponse ~ Environment1, data = testData2)
 glmm1 <- lme4::glmer(observedResponse ~ Environment1 + (1|group), data = testData2,
-            family = "poisson")
+                     family = "poisson")
 
 
 # 1. Likelihood ratio tests ----
@@ -48,13 +48,15 @@ performance::check_overdispersion(glm1)
 
 msme::P__disp(glm1)
 
-RVAideMemoire:: overdisp.glmer(glmm1)
+RVAideMemoire::overdisp.glmer(glmm1)
 
 overdisp::overdisp(testData2, 2, 3)
 
 AER::dispersiontest(glm1)
 
+aods3::gof(glm1)
 
+blmeco::dispersion_glmer(glmm1)
 
 # 4. Response variance ----
 
@@ -89,7 +91,3 @@ qtest1 <- qcc.overdispersion.test(testData1$observedResponse)
 qtest1
 qtest2 <- qcc.overdispersion.test(testData2$observedResponse)
 qtest2
-
-
-
-
