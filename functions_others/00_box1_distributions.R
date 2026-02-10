22412b023de0fe15b0c7d32055b214048d1e5004
melina-leite <melinasouzaleite@gmail.com>
2026-02-10 15:54:56 +0100

    update in box 1 figure and rearranging files

--- functions_others/00_box1_distributions.R
+++ functions_others/00_box1_distributions.R
@@ -36,3 +36,29 @@ ggplot(distribs, aes(x=values, fill=distrib)) + geom_bar() +
             aes(x = x, y = y, label = label))
   
 ggsave("figures/box1_distributions.png", heigh=9, width = 28, units="cm")
+
+
+
+library(glmmTMB)
+library(DHARMa)
+
+m1 <- glmmTMB(values ~ 1, family=compois(), data=distribs[distribs$distrib=="Conway-Maxwell-Poisson",])
+summary(m1)
+exp(fixef(m1)[[1]])
+res1 <- simulateResiduals(m1)
+testDispersion(res1, type="PearsonChisq")
+testDispersion(res1)
+
+
+m2 <- glmmTMB(values ~ 1, family=poisson(), data=distribs[distribs$distrib=="Poisson",])
+exp(fixef(m2)[[1]])
+res2 <- simulateResiduals(m2)
+testDispersion(res2, type="PearsonChisq")
+testDispersion(res2)
+
+m3 <- glmmTMB(values ~ 1, family=nbinom2(), data=distribs[distribs$distrib=="Negative Binomial",])
+summary(m3)
+exp(fixef(m3)[[1]])
+res3 <- simulateResiduals(m3)
+testDispersion(res3, type="PearsonChisq")
+testDispersion(res3)
